7dd7a4519543d25e7168e9a61f4814e1455ebd1e
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2016-08-22 12:39:19 -0500

    Adding project with first 2 tutorials
    Adding the first 2 tutorials

---
+++ src/AngularAspCoreTutorial/Controllers/TutorialController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AngularAspCoreTutorial.Controllers
+{
+    public class TutorialController : Controller
+    {
+        public IActionResult Requirements()
+        {
+            return View();
+        }
+
+        public IActionResult CreateNewProject()
+        {
+            return View();
+        }
+    }
+}
